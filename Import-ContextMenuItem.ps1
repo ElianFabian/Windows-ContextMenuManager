d8d6f3e6f16b39d888ef766418cc1c5ca1f385fb
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2022-10-27 20:16:02 +0200

    Update Import-ContextMenuItem.ps1

--- Import-ContextMenuItem.ps1
+++ Import-ContextMenuItem.ps1
@@ -7,4 +7,4 @@ Import-Module -Name "$PSScriptRoot/Modules/ContextMenuManager.psm1"
 Start-ContextMenuProcess `
     -FunctionName "Import-ContextMenuItem" `
     -ArgumentList "-NonInteractive" `
-    -Message "Importing the context menus into the registry..."
+    -Message "Importing the context menus items into the registry..."
